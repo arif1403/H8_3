71f1e79fdc1832aade8d9992fc5ca5ed1f526afe
arif1403 <arifmunandar.che@gmail.com>
2022-10-16 08:22:52 +0700

    whatever lah

--- Pertemuan 6/Arif Munandar [chellenge 2].py
+++ Pertemuan 6/Arif Munandar [chellenge 2].py
@@ -2,22 +2,24 @@ print("+" *44)
 print("+ Program Mengubah Angka Menjadi Terbilang +")
 print("+"*44)
 
+
 def Terbilang(bil):
     angka = ["Nol","Satu","Dua","Tiga","Empat","Lima","Enam","Tujuh","Delapan",
-            "Sembilan","Sepuluh","Sebelas"]
+             "Sembilan", "Sepuluh", "Sebelas"]
     result = " "
     n = int(bil)
     if n >= 0 and n <= 11:
         result = result + angka[n]
     elif n < 20:
-        result = Terbilang(n%10) + " Belas"
+        result = Terbilang(n % 10) + " Belas"
     elif n < 100:
-        result = Terbilang(n/10) + " Puluh" + Terbilang(n%10)
+        result = Terbilang(n/10) + " Puluh" + Terbilang(n % 10)
     else:
         result = "Melebihi Batas dan Melampauinya"
     return result
 
+
 if __name__ == '__main__':
     bil = input("Masukkan angka (0-99) : ")
-    tbl = Terbilang(bil )
+    tbl = Terbilang(bil)
     print(tbl)
